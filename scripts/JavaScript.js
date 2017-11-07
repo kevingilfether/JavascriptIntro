73ecf2b445d4d7ddb308ed5bd4c91ef0d89df4b8
kevingilfether <kevingilfether@gmail.com>
2017-11-07 11:03:31 -0500

    Wrote some javascript functions, called

--- scripts/JavaScript.js
+++ scripts/JavaScript.js
@@ -1,4 +1,29 @@
-﻿for (var i = 0; i < 10; i++)
-{
-    console.log(i)
+﻿//console.log("Hello World!");
+//var name = "Kevin";
+
+//console.log("Hello " + name);
+
+////writing a loop to count from 1-10
+
+//for (var i = 1; i < 11; i++)
+//{
+//    console.log(i);
+//}
+
+//Starting to talk about JavaScript Functions
+
+//function helloWorld() {
+//    var greeting = "Hello world!";
+//    alert(greeting);
+//}
+
+//helloWorld();
+
+function add(a, b) {
+    return a + b;
 }
+
+console.log(add(4,1000000000))
+
+
+
